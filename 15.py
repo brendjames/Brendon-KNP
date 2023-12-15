8853b46cd8749e4d3207976e5c7739f3db42acd7
Brendon Kirk <99794807+brendjames@users.noreply.github.com>
2023-12-15 11:32:31 +0100

    Refactor code for improved performance

--- 15.py
+++ 15.py
@@ -8,45 +8,33 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 def scrape_tweets():
-    # Setup the WebDriver
     driver = webdriver.Chrome()
-
-    # Navigate to the page
     driver.get('https://nitter.net/LatestKruger')
+    time.sleep(10)
 
-    # Wait for the page to load
-    time.sleep(10)  # Not ideal, consider using WebDriverWait
-
-    # Open the CSV file to write data
-    with open('Dec15.csv', 'a', newline='', encoding='utf-8') as f:
+    with open('Dec15v1.csv', 'a', newline='', encoding='utf-8') as f:
         csv_writer = csv.writer(f)
-        # Write headers to the CSV file
         csv_writer.writerow(['ID', 'Time', 'Animal Type and Number', 'Location', 'Rating', 'Tinged By', 'Title', 'Tweet Date'])
-        
-        # Define the ID outside the page processing loop    
-        i = 1
 
-        for _ in range(100):  # Repeat the process 10 times
-            # Scroll down until new content stops loading or a certain condition is met
+        i = 1  # Define 'i' outside the loop for row numbering
+
+        for _ in range(100):  # Repeat the process 100 times
             last_height = driver.execute_script("return document.body.scrollHeight")
 
             while True:
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-                time.sleep(5)  # Adjust this value based on the page loading speed
+                time.sleep(5)
 
                 new_height = driver.execute_script("return document.body.scrollHeight")
                 if new_height == last_height:
                     break
                 last_height = new_height
 
-            # Get the updated HTML content after loading more tweets
             html = driver.page_source
             soup = BeautifulSoup(html, 'html.parser')
-
-            # Find all tweet content divs
             tweet_content_divs = soup.find_all('div', class_='tweet-content media-body')
 
-            for i, tweet_content_div in enumerate(tweet_content_divs, start=1):
+            for row_number, tweet_content_div in enumerate(tweet_content_divs, start=i):
                 if tweet_content_div is not None:
                     # Extracting different parts of the tweet
                     tweet_content = tweet_content_div.get_text(strip=True)
@@ -64,7 +52,10 @@ def scrape_tweets():
                     tinger = tinger.split('Tinged by ')[-1]
                     location = ' '.join(lines[2:-2])
 
-                    # Extracting tweet date for each tweet content div
+                    # Filtering locations starting with 'H' or 'S'
+                    if not (location.startswith('H') or location.startswith('S')):
+                        continue  # Skip if location doesn't start with 'H' or 'S'
+
                     tweet_div = tweet_content_div.parent
                     tweet_date_span = tweet_div.find('span', class_='tweet-date')
                     title = ""
@@ -73,24 +64,23 @@ def scrape_tweets():
                         tweet_date_a = tweet_date_span.find('a')
                         if tweet_date_a:
                             title = tweet_date_a.get('title')
-                            tweet_date = tweet_date_a.text  # Extracting the tweet date text
+                            tweet_date = tweet_date_a.text
 
-                    # Write extracted data to CSV
-                    csv_writer.writerow([i, tweet_time, animal_type_and_number, location, rating, tinger, title, tweet_date])
+                    csv_writer.writerow([row_number, tweet_time, animal_type_and_number, location, rating, tinger, title, tweet_date])
 
-            # Finding a 'Next' button and clicking it
             try:
                 next_page_link = WebDriverWait(driver, 30).until(EC.element_to_be_clickable((By.LINK_TEXT, 'Load more')))
                 next_page_link.click()
-                time.sleep(5)  # Wait for the new content to load
+                time.sleep(5)
             except Exception as e:
                 print("No more 'Load more' button found.")
                 break
 
-            # Wait for the appearance of elements related to newly loaded content
             WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.CLASS_NAME, "tweet-content")))
 
-    driver.quit()  # Close the WebDriver once done
+            i = row_number + 1  # Update 'i' to the next row number for the next iteration
+
+    driver.quit()
 
 if __name__ == "__main__":
     scrape_tweets()
